c3fdb45b08fb8b631c3a8eb5985c1ac84a130101
Halit Akca <halitakca070@gmail.com>
2023-08-30 20:58:57 +0300

    İlham Ver Design Application is built.

--- app/src/main/java/com/halitakca/jetpackcomposetasarim/MainActivity.kt
+++ app/src/main/java/com/halitakca/jetpackcomposetasarim/MainActivity.kt
@@ -1,5 +1,98 @@
 package com.halitakca.jetpackcomposetasarim
 
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.halitakca.jetpackcomposetasarim.ui.theme.JetpackComposeTasarimTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeTasarimTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Sayfa()
+                }
+            }
+        }
+    }
+}
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun Sayfa(){
+    Scaffold(
+        topBar ={
+                TopAppBar(title ={
+                    Text(text = "İlham Ver") },
+                    backgroundColor = colorResource(id = R.color.anaRenk),
+                    contentColor = colorResource(id = R.color.white)
+                )
+        },
+        content = {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.SpaceEvenly,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Image(painter = painterResource(id = R.drawable.stevejobs),
+                        contentDescription = "")
+                    Text(text = "Steve Jobs", color = Color.Red,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 22.sp
+                    )
+                }
+                Text(text = "Dünyayı değiştirecek insanlar, onu değiştirebileceklerini düşünecek kadar çılgın olanlardır.",
+                modifier = Modifier.padding(10.dp),
+                textAlign = TextAlign.Center)
+
+                Button(onClick = { Log.e("Button","İlham verildi")},
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = Color.Red,
+                    contentColor = Color.White
+                )) {
+                    Text(text = "İLHAM VER")
+                }
+            }
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetpackComposeTasarimTheme {
+        Sayfa()
+    }
+}
+
+/*
+package com.halitakca.jetpackcomposetasarim
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -41,3 +134,4 @@ fun DefaultPreview() {
         Greeting("Android")
     }
 }
+ */
